6c314fda03ca43040e6af0099ac848ae612fe8e6
sush <sushmitha407@gmail.com>
2022-04-26 00:58:33 -0400

    commit Mains

---
+++ src/Mains.java
@@ -0,0 +1,19 @@
+
+enum Level{
+
+    LOW,MEDIUM,HIGH
+}
+
+public class Mains{
+    public static void main(String[]args){
+
+        for (Level myvar:Level.values()){
+
+            System.out.println(myvar);
+        }
+    }
+
+
+}
+
+
